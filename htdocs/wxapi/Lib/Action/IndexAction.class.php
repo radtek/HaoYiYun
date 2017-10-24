ff2388d016e1c7b0e7111a7c3ed5ccb4234b4dd9
18059104@qq.com <huijia264>
2017-10-24 22:12:43 +0800

    新增配合安装脚本的php代码

--- htdocs/wxapi/Lib/Action/IndexAction.class.php
+++ htdocs/wxapi/Lib/Action/IndexAction.class.php
@@ -31,5 +31,31 @@ class IndexAction extends Action
     //print_r($this->m_detect->getHttpHeaders());
     //print_r($_COOKIE); exit;
   }
+  //
+  // 处理配置存盘接口...
+  // tracker/x.x.x.x:22122/transmit/x.x.x.x:21001
+  public function config()
+  {
+    // 获取并解析传递过来的参数信息...
+    $arrTracker = explode(':', $_GET['tracker']);
+    $arrTransmit = explode(':', $_GET['transmit']);
+    if( count($arrTracker) != 2 || count($arrTransmit) != 2 ) {
+      echo "== tracker or transmit must be set ===\n";
+      exit;
+    }
+    // 获取系统配置记录...
+    $dbSys = D('system')->field('system_id')->find();
+    // 将获取到的数据组合成存盘数据...
+    $dbSys['sys_site'] = "https://www.myhaoyi.com";
+    $dbSys['web_tracker_addr'] = "http://" . $arrTracker[0];
+    $dbSys['tracker_addr'] = $arrTracker[0];
+    $dbSys['tracker_port'] = $arrTracker[1];
+    $dbSys['transmit_addr'] = $arrTransmit[0];
+    $dbSys['transmit_port'] = $arrTransmit[1];
+    // 保存到系统表当中...
+    D('system')->save($dbSys);
+    // 打印成功信息...
+    echo "== [OK] ==\n";
+  }
 }
 ?>
