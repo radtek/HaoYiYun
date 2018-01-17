2a98898b7364649f5f18756fddb6d69b04615eb7
18059104@qq.com <huijia264>
2018-01-17 16:28:52 +0800

    将版本升级到了1.2.1

--- monitor/wxapi/Conf/config.php
+++ monitor/wxapi/Conf/config.php
@@ -6,7 +6,7 @@
   //设定项目配置
   $array = array(
     // 定义版本号...
-    'VERSION' => '1.2.0',
+    'VERSION' => '1.2.1',
     // 微信登录网站应用需要的参数配置 => 微信开放平台
     'WECHAT_LOGIN' => array(
       'scope'=>'snsapi_login', //用户授权的作用域,使用逗号(,)分隔
