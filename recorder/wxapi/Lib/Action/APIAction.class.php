4f9c9f45bf6e4d6fa723df535402969dccd42f3b
18059104@qq.com <huijia264>
2018-01-26 19:06:14 +0800

    修正了 play_camera 和 play_record 的API接口错误；

--- recorder/wxapi/Lib/Action/APIAction.class.php
+++ recorder/wxapi/Lib/Action/APIAction.class.php
@@ -1146,7 +1146,7 @@ class APIAction extends Action
       // 没有设置宽度，默认850px，没有设置高度，默认550px
       $theWidth = (isset($_GET['width']) ? $_GET['width'] : 850);
       $theHeight = (isset($_GET['height']) ? $_GET['height'] : 550);
-      // 组合需要返回的播放地址内容...
+      // 组合需要返回的播放地址内容 => Home
       $strUrl = sprintf("%s://%s/wxapi.php/Home/show/type/live/camera_id/%d/width/%d/height/%d", 
                         $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST'],
                         $camera_id, $theWidth, $theHeight); 
@@ -1172,7 +1172,7 @@ class APIAction extends Action
       // 没有设置宽度，默认850px，没有设置高度，默认550px
       $theWidth = (isset($_GET['width']) ? $_GET['width'] : 850);
       $theHeight = (isset($_GET['height']) ? $_GET['height'] : 550);
-      // 组合需要返回的播放地址内容...
+      // 组合需要返回的播放地址内容 => Home
       $strUrl = sprintf("%s://%s/wxapi.php/Home/show/type/vod/record_id/%d/width/%d/height/%d", 
                         $_SERVER['REQUEST_SCHEME'], $_SERVER['HTTP_HOST'],
                         $record_id, $theWidth, $theHeight); 
